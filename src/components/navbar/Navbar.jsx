3118c0226cdbd5db078aaa379f66a7452d3557bb
Jaroslaw Charchula <charchula@gmail.com>
2022-09-17 16:37:48 +0200

    deleted app css file add png netflixLogoT add global settings in index css edit Home pages edit Navbar component and css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Navbar.css';
-import Netflix from '../../assets/netflixLogo.png';
+import Netflix from '../../assets/netflixLogoT.png';
 import netflixAvatar from '../../assets/netflixAvatar.png';
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={Netflix} alt='logo' className='navbar__logo' />
-      <img
-        src={netflixAvatar}
-        alt='personal avatar'
-        className='navbar__avatar'
-      />
-      <h1>navbar</h1>
+    <div className='navbar navbar__black'>
+      <div className='navbar__contens'>
+        <img src={Netflix} alt='logo' className='navbar__logo' />
+        <img
+          src={netflixAvatar}
+          alt='personal avatar'
+          className='navbar__avatar'
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,6 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='home'>
-      Home
       <Navbar />
       {/* banner */}
       {/* rows */}
