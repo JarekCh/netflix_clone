e76e6b5e91911719399756b85a6998509ed6e625
Jaroslaw Charchula <charchula@gmail.com>
2022-09-17 17:17:20 +0200

    add navbar logi for background add navbar css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import Netflix from '../../assets/netflixLogoT.png';
 import netflixAvatar from '../../assets/netflixAvatar.png';
 
 const Navbar = () => {
+  const [handleShow, setHandleShow] = useState(false);
+  console.log(
+    '🚀 ~ file: Navbar.jsx ~ line 8 ~ Navbar ~ handleShow',
+    handleShow
+  );
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      setHandleShow(true);
+    } else {
+      setHandleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavbar);
+    return () => {
+      window.removeEventListener('scroll', transitionNavbar);
+    };
+  }, []);
+
   return (
-    <div className='navbar navbar__black'>
+    <div className={`navbar ${handleShow && 'navbar__black'}`}>
       <div className='navbar__contens'>
         <img src={Netflix} alt='logo' className='navbar__logo' />
         <img
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,18 @@ import Navbar from '../../components/navbar/Navbar';
 import './Home.css';
 
 const Home = () => {
+  const rows = () => {
+    const row = [];
+    for (let i = 0; i < 60; i++) {
+      row.push(<h1 key={i}>test</h1>);
+    }
+    return row;
+  };
+
   return (
     <div className='home'>
       <Navbar />
+      <div>{rows()}</div>
       {/* banner */}
       {/* rows */}
     </div>
