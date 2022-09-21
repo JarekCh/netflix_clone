fc7d2910f9f9cffcb86e324476bd67c3df7ffb37
Jaroslaw Charchula <charchula@gmail.com>
2022-09-21 23:34:10 +0200

    edit Banner and Navbar css, add icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Netflix from '../../assets/netflixLogoT.png';
 import netflixAvatar from '../../assets/netflixAvatar.png';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { BiBell } from 'react-icons/bi';
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
   const [handleShow, setHandleShow] = useState(false);
@@ -28,18 +29,30 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className={`navbar ${handleShow && 'navbar__black'}`}>
+    <nav className={`navbar ${handleShow && 'navbar__black'}`}>
       <div className='navbar__contens'>
-        <IoMdArrowDropdown />
-        <BiBell />
-        <img src={Netflix} alt='logo' className='navbar__logo' />
-        <img
-          src={netflixAvatar}
-          alt='personal avatar'
-          className='navbar__avatar'
-        />
+        <div className='navbar__left'>
+          <img src={Netflix} alt='logo' className='navbar__logo' />
+        </div>
+        <div className='navbar__right'>
+          <span className='navbar__rightSearch icons'>
+            <FaSearch />
+          </span>
+          <span>Kids</span>
+          <span className='navbar__rightBell icons'>
+            <BiBell />
+          </span>
+          <div className='navbar__profileLink'>
+            <img
+              src={netflixAvatar}
+              alt='personal avatar'
+              className='navbar__avatar'
+            />
+            <span>▼</span>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
