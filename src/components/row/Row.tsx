0d82b1764fb62a31302640543efb1dade62098af
Jaroslaw Charchula <charchula@gmail.com>
2023-08-16 23:11:56 +0200

    add map func to row component

--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -10,8 +10,10 @@ type Props = {
     isLargeRow?: boolean;
 }
 
-const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
-    const [movies, setMovies] = useState<Movie[] | []>([]);
+const Row = ({ title, fetchUrl, isLargeRow = false }: Props) => {
+    const [movies, setMovies] = useState<Movie[] | null>(null);
+
+    const base_url = 'https://image.tmdb.org/t/p/original/';
 
     useEffect(() => {
         async function fetchData() {
@@ -26,6 +28,12 @@ const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     return (
         <div className='row'>
             <h2>{title}</h2>
+            {movies?.map(movie => {
+                return <img className={`row_poster ${isLargeRow && 'row__posterLarge'}`}
+                    key={movie.id}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                    alt={movie.name} />
+            })}
         </div>
     )
 }
