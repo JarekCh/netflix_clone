b7562b79802101e2b3829eca2633670ae8e01ee6
Jaroslaw Charchula <charchula@gmail.com>
2023-08-19 14:49:19 +0200

    install react oruter library add first routes

---
+++ README.md
@@ -0,0 +1,1 @@
+https://netflix-clone-45335.web.app/
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Home from './pages/Home/Home';
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className='app'>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
