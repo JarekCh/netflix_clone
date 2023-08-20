5de92fba6d8efd1db3b1f5892776dd6074700b6a
Jaroslaw Charchula <charchula@gmail.com>
2023-08-21 00:28:54 +0200

    edit login component and css

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,9 +8,25 @@ const Login = (props: Props) => {
     <div className="login">
       <div className="login__background">
         <img className="login__logo" src={Netflix} alt=""></img>
+        <button className="login__button">Sign In</button>
+        <div className="login__gradient"></div>
       </div>
-      <button className="login__button">Sign In</button>
-      <div className="login__gradient"></div>
+      <div className="login__body">
+        <>
+          <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhare. Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="login__input">
+            <form>
+              <input type="email" placeholder="Email Adress" />
+              <button className="login__getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
     </div>
   );
 };
