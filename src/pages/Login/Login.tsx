cb8112fee0eb8aa241942e41df77301f01c6483c
Jaroslaw Charchula <charchula@gmail.com>
2023-08-20 23:31:18 +0200

    add login css

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,16 @@
 import './Login.css';
+import Netflix from '../../assets/netflixLogoT.png';
 
 type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <div className="Login">
-      <div className="login__background"></div>
+    <div className="login">
+      <div className="login__background">
+        <img className="login__logo" src={Netflix} alt=""></img>
+      </div>
+      <button className="login__button">Sign In</button>
+      <div className="login__gradient"></div>
     </div>
   );
 };
