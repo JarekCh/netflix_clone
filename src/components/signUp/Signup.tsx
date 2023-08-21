a605eafc231c2b6c99a9add9293020aecd776a86
Jaroslaw Charchula <charchula@gmail.com>
2023-08-21 22:56:01 +0200

    edit login component and css create singup component and css file

---
+++ src/components/signUp/Signup.tsx
@@ -0,0 +1,9 @@
+import './SignUp.css';
+
+type Props = {};
+
+const Signup = (props: Props) => {
+  return <div className="singup">Signup</div>;
+};
+
+export default Signup;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,31 +1,46 @@
 import './Login.css';
 import Netflix from '../../assets/netflixLogoT.png';
+import { useState } from 'react';
+import Signup from '../../components/signUp/Signup';
 
 type Props = {};
 
 const Login = (props: Props) => {
+  const [signIn, setSignIn] = useState<boolean>(false);
+
   return (
     <div className="login">
       <div className="login__background">
         <img className="login__logo" src={Netflix} alt=""></img>
-        <button className="login__button">Sign In</button>
+        <button onClick={() => setSignIn(true)} className="login__button">
+          Sign In
+        </button>
         <div className="login__gradient"></div>
       </div>
       <div className="login__body">
-        <>
-          <h1>Unlimited films, TV programmes and more.</h1>
-          <h2>Watch anywhare. Cancel at any time.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </h3>
-          <div className="login__input">
-            <form>
-              <input type="email" placeholder="Email Adress" />
-              <button className="login__getStarted">GET STARTED</button>
-            </form>
-          </div>
-        </>
+        {signIn ? (
+          <Signup />
+        ) : (
+          <>
+            <h1>Unlimited movies, TV shows, and more</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="login__input">
+              <form>
+                <input type="email" placeholder="Email Adress" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="login__getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
