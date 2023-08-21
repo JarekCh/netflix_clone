1397cb334737f9c1fca5306c339fdabdf57b42a8
Jaroslaw Charchula <charchula@gmail.com>
2023-08-22 00:12:24 +0200

    add Signup css add signUp jsx

--- src/components/signUp/Signup.tsx
+++ src/components/signUp/Signup.tsx
@@ -3,7 +3,31 @@ import './SignUp.css';
 type Props = {};
 
 const Signup = (props: Props) => {
-  return <div className="singup">Signup</div>;
+  const register = (e: Event) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e: Event) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="singup">
+      <form>
+        <h1>Sing In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+
+        <h4>
+          <span className="signup__gray">New to Netflix?</span>
+          <span className="signup__link"> Sign up now.</span>
+        </h4>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
