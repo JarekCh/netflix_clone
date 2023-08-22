f0fd5f6717ef0a20147b3e42bdc5dec40bb0bcf2
Jaroslaw Charchula <charchula@gmail.com>
2023-08-23 00:21:11 +0200

    add auth ligic to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import Home from './pages/Home/Home';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //log in
+        console.log(userAuth);
+      } else {
+        //log off
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
