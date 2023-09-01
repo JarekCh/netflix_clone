0d49d6c53fa99a734465d04eedd4b0b7750b940c
Jaroslaw Charchula <charchula@gmail.com>
2023-09-01 21:52:58 +0200

    move images to public file add global style edit jubmo.json add Jumbotron styles

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -1,5 +1,6 @@
 import './Jumbotron.css';
 import { FlexDirection } from '../../utils/shared/types';
+import useMediaQuery from '../../utils/hooks/useMadiaQuery';
 
 type Props = {
   title: string;
@@ -10,14 +11,21 @@ type Props = {
 };
 
 const Jumbotron = ({ title, subTitle, image, alt, direction }: Props) => {
+  const isAboveMediumScreens = useMediaQuery('(min-width: 900px)');
+
   return (
-    <div className="jumbotron" style={{ flexDirection: direction }}>
-      <div className="jumbotron__contentWraper">
-        <h1>{title}</h1>
-        <h2>{subTitle}</h2>
-      </div>
-      <div className="jumbotron__contentWraper">
-        <img className="jumbotron__img" src={image} alt={alt} />
+    <div className="jumbotron__border">
+      <div
+        className="jumbotron"
+        style={{ flexDirection: isAboveMediumScreens ? direction : 'column' }}
+      >
+        <div className="jumbotron__contentWraper">
+          <h1>{title}</h1>
+          <h2>{subTitle}</h2>
+        </div>
+        <div className="jumbotron__contentWraper">
+          <img className="jumbotron__img" src={image} alt={alt} />
+        </div>
       </div>
     </div>
   );
